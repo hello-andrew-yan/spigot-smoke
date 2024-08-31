dc1ea99ace0ca85e19bde1ebebe8fa12bd45c87a
Andrew <hello.andrew.yan@outlook.com>
2024-08-31 17:19:12 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spigot-smoke
+ Tutorial on how to implement a smoke in Minecraft using the SpigotAPI.
---
+++ src/main/java/io/github/helloandrewyan/smoke/Smoke.java
@@ -0,0 +1,17 @@
+package io.github.helloandrewyan.smoke;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Smoke extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        tEST
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
